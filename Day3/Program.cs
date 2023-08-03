e45e00f58f5260ca01cc6358f61699f3c8476efd
Amanuel Wubete <aman.alehegne2@gmail.com>
2023-08-03 19:13:09 +0300

    Update

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -4,13 +4,15 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        TaskClass task = new TaskClass { Name = "Task4", Description = "Lorem Ibsem", GetCategory = (Category)0, IsCompleted = true };
+        
+        TaskClass task = new TaskClass { Name = "Task69", Description = "Lorem Ibsem", GetCategory = (Category)4, IsCompleted = false };
         TaskManager manager = new TaskManager { FilePath = "/Users/amanuelwubete/Desktop/file.csv"};
 
-        //await manager.WriteToFileAsync(task);
-        await manager.ReadFileAsync();
-        await manager.FilterFileAsync((Category)10);
-
+        // await manager.WriteToFileAsync(task);
+        await manager.ReadFileAsync(true);
+        await manager.UpdateFileAsync("Task0", task);
+        await manager.FilterFileAsync((Category)1)
+        await manager.ReadFileAsync(true);
     }
 }
 
